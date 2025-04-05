91c6db00d331396bd7d04dec8f07842610b2738a
Leonn190 <euleonsoto@gmail.com>
2025-04-04 22:59:53 -0300

    adiçao players

--- jogo.py
+++ jogo.py
@@ -4,6 +4,12 @@ def golpe_de_fogo(atacante, alvo):
     dano = 50 - alvo["Def"]
     alvo["Vida"] -= dano
 
+def Disparo_quente(atacante, alvo):
+    if alvo['Def'] > 10:
+        alvo['Def'] = 10
+    dano = 10 - alvo["Def"]
+    alvo["Vida"] -= dano
+
 def fogo_puro(atacante, alvo):
     if alvo['Def SP'] > 60:
         alvo['Def SP'] = 60
@@ -28,36 +34,64 @@ pokemon1 = {
 }
 
 pokemon2 = {
-    "nome": "Bulbasaur",
+    "nome": "Litten",
     "Vida": 120,
     "Def": 40,
     "Def SP": 0,
+    "Velocidade": 3,
+    "ataque 1": Disparo_quente,
+    "ataque 2": defesa_flamejante
+}
+
+pokemon3 = {
+    "nome": "Fenikin",
+    "Vida": 90,
+    "Def": 30,
+    "Def SP": 20,
+    "Velocidade": 3,
+    "ataque 1": Disparo_quente,
+    "ataque 2": fogo_puro
+}
+
+pokemon4 = {
+    "nome": "Slugma",
+    "Vida": 130,
+    "Def": 10,
+    "Def SP": 30,
     "Velocidade": 3,
     "ataque 1": golpe_de_fogo,
     "ataque 2": defesa_flamejante
 }
 
-def opções(player, inimigo):
-    desejo = input(f"O que {player['nome']} deseja fazer? Atacar, Mover ou Passar o turno? ")
+
+player1 = ["leon",pokemon1,pokemon2]
+player2 = ["jonas",pokemon3,pokemon4]
+
+def opções_de_pokemon(player, inimigo):
+    pokemon_escolhido = int(input("Qual pokemon voce deseja escolher? O 1 ou 2"))
+    pokemon = player[pokemon_escolhido]
+    desejo = input(f"O que {pokemon['nome']} deseja fazer? Atacar, Mover ou Passar o turno?")
     if desejo == "atacar":
-        atacar(player, inimigo)
+        atacar(pokemon, inimigo, player)
     elif desejo == "mover":
         print("Movimentação ainda não configurada.")
     elif desejo == "passar o turno":
         passar_o_turno(inimigo, player)
 
 def passar_o_turno(player, inimigo):
-    print(f"\n--- Novo turno de {player['nome']} ---")
-    opções(player, inimigo)
+    print(f"Novo turno de {player[0]}")
+    opções_de_pokemon(player, inimigo)
 
-def atacar(player, inimigo):
+def atacar(pokemon, inimigo, player):
+    alvo_escolhido = int(input("escolha seu alvo, 1 ou 2"))
+    alvo = inimigo[alvo_escolhido]
     ataque = input("Quer usar o ataque 1 ou o ataque 2? ")
-    if ataque in player:
-        player[ataque](player, inimigo)
-        print(f"A vida atual de {inimigo['nome']} é {inimigo['Vida']}")
+    if ataque in pokemon:
+        pokemon[ataque](pokemon, alvo)
+        print(f"A vida atual de {alvo['nome']} é {alvo['Vida']}")
     else:
         print("Ataque inválido!")
 
     passar_o_turno(inimigo, player)
 
-opções(pokemon1, pokemon2)
+opções_de_pokemon(player1, player2)
