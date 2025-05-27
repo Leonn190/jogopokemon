cefc63190b84cb74bb6789afe4f16cf9fa122378
Leonn190 <euleonsoto@gmail.com>
2025-05-27 20:33:12 -0300

    API up

--- ConfigFixa.py
+++ ConfigFixa.py
@@ -1,3 +1,3 @@
 # Arquivo gerado automaticamente para armazenar configurações fixas
 
-Config = {'Volume': 0.8, 'FPS': 148.38805970149252, 'Claridade': 41.7910447761194, 'Modo rápido': False, 'Dicas': False, 'Modo silencioso': False, 'Mostrar Fps': False, 'Versão': 'Beta 1.2.0', 'Modo': None}
+Config = {'Volume': 0.8, 'FPS': 148.38805970149252, 'Claridade': 41.7910447761194, 'Modo rápido': True, 'Dicas': False, 'Modo silencioso': False, 'Mostrar Fps': False, 'Versão': 'Beta 1.2.0', 'Modo': None}
